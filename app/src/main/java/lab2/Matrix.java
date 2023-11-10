a7f29209d09aa2c6055af7ea24b22e2eaa91cbad
Andrew Yavorskyi <aavorskij7@gmail.com>
2023-11-10 19:23:26 +0200

    step 1

---
+++ app/src/main/java/lab2/Matrix.java
@@ -0,0 +1,13 @@
+package lab2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Matrix {
+
+    private int rows;
+    private int columns;
+    private Double[][] data;
+    private Double[][] dataCopy;
+
+}
